0eef1cc2b72ba0647271b017fa55a97746cf3db1
GonzaCarry <gonzag0139@gmail.com>
2018-11-09 16:09:45 +0000

    Prueba1

---
+++ js/puzzle.js
@@ -0,0 +1,69 @@
+var piezas = document.getElementsByClassName("movil");
+
+for(var i=0;i<piezas.length;i++){
+	piezas[i].setAttribute("x", Math.floor((Math.random() * 10) + 1));
+	piezas[i].setAttribute("y", Math.floor((Math.random() * 409) + 1));
+	piezas[i].setAttribute("onmousedown","seleccionarElemento(evt)");
+}
+
+var elementSelect = 0;
+var currentX = 0;
+var currentY = 0;
+var currentPosX = 0;
+var currentPosY = 0;
+
+function seleccionarElemento(evt) {
+	elementSelect = reordenar(evt);
+	currentX = evt.clientX;
+	currentY = evt.clientY;
+	currentPosx = parseFloat(elementSelect.getAttribute("x"));
+	currentPosy = parseFloat(elementSelect.getAttribute("y"));
+	elementSelect.setAttribute("onmousemove","moverElemento(evt)");
+}
+
+function moverElemento(evt){
+	var dx = evt.clientX - currentX;
+	var dy = evt.clientY - currentY;
+	currentPosx = currentPosx + dx;
+	currentPosy = currentPosy + dy;
+	elementSelect.setAttribute("x",currentPosx);
+	elementSelect.setAttribute("y",currentPosy);
+	currentX = evt.clientX;
+	currentY = evt.clientY;
+	elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)");
+	elementSelect.setAttribute("onmouseup","deseleccionarElemento(evt)");
+	iman();
+}
+
+function deseleccionarElemento(evt){
+	testing(evt);
+	if(elementSelect != 0){
+		elementSelect.removeAttribute("onmousemove");
+		elementSelect.removeAttribute("onmouseout");
+		elementSelect.removeAttribute("onmouseup");
+		elementSelect = 0;
+	}
+}
+
+var entorno = document.getElementById("entorno");
+
+function reordenar(evt){
+	var pieza = evt.target.parentNode;
+	var clone = pieza.cloneNode(true);
+	var id = pieza.getAttribute("id");
+	entorno.removeChild(document.getElementById(id));
+	entorno.appendChild(clone);
+	return entorno.lastChild.firstChild;
+}
+
+var origX = [205,400,598,205,400,598,205,400,598];
+var origY = [100,100,100,300,300,300,500,499,500];
+
+function iman(){
+	for(var i=0;i<piezas.length;i++){
+		if (Math.abs(currentPosx-origX[i])<15 && Math.abs(currentPosy-origY[i])<15) {
+			elementSelect.setAttribute("x",origX[i]);
+			elementSelect.setAttribute("y",origY[i]);
+		}
+	}
+}
